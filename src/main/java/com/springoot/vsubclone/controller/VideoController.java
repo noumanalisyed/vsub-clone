30493bdccb833d6fb12e74485922ab8fdcd4b0e7
infiniV <raahim.arbaz22@gmail.com>
2024-07-06 00:23:03 +0500

    feature issue 2 - repair - api rem - img_gen

--- src/main/java/com/springoot/vsubclone/controller/VideoController.java
+++ src/main/java/com/springoot/vsubclone/controller/VideoController.java
@@ -5,6 +5,7 @@ import com.springoot.vsubclone.controller.response.VideoResponse;
 import com.springoot.vsubclone.service.VideoGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -15,11 +16,15 @@ public class VideoController {
 
     @Autowired
     private VideoGenerationService videoGenerationService;
+    @Autowired
+    private StringHttpMessageConverter stringHttpMessageConverter;
 
     @PostMapping("/generate_video")
     public VideoResponse generateVideo(@RequestBody PromptRequest request) {
         try {
             String script = videoGenerationService.generateScriptFromPrompt(request.getPrompt());
+            String img_prompt = videoGenerationService.extractTextFromScript(script);
+            String img_path = videoGenerationService.generateImageFromScript(img_prompt);
             String videoPath = videoGenerationService.generateVideoFromScript(script);
             return new VideoResponse(videoPath);
         } catch (IOException e) {
--- src/main/java/com/springoot/vsubclone/service/VideoGenerationService.java
+++ src/main/java/com/springoot/vsubclone/service/VideoGenerationService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -52,6 +54,66 @@ public class VideoGenerationService {
             return jsonNode.get("choices").get(0).get("message").get("content").asText();
         }
     }
+    //a function to only extract text from [] in the script
+    public String extractTextFromScript(String script) {
+        StringBuilder extractedText = new StringBuilder();
+        for (String line : script.split("\n")) {
+            if (line.startsWith("[")) {
+                extractedText.append(line).append("\n");
+            }
+        }
+        //save text to a file named img-prompts.txt
+        try {
+            java.nio.file.Files.writeString(new File("img-prompts.txt").toPath(), extractedText.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //print the extracted text
+        System.out.println(extractedText.toString());
+        return extractedText.toString();
+    }
+    //generate images from script.txt using DALL.E API
+    public String generateImageFromScript(String script) throws IOException {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("prompt", "make an stylistic image that suits the topic : "+ script + "realistic and detailed and high quality image.");
+        jsonMap.put("n", 1);
+        jsonMap.put("size", "512x512");
+
+        String json = objectMapper.writeValueAsString(jsonMap);
+
+        RequestBody body = RequestBody.create(json, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/images/generations")
+                .header("Authorization", "Bearer " + apiKey)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+            String responseBody = response.body().string();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+            String imageUrl = jsonNode.get("data").get(0).get("url").asText();
+
+            Request imageRequest = new Request.Builder().url(imageUrl).build();
+            try (Response imageResponse = client.newCall(imageRequest).execute()) {
+                if (!imageResponse.isSuccessful()) {
+                    throw new IOException("Unexpected code " + imageResponse);
+                }
+                InputStream imageStream = imageResponse.body().byteStream();
+                File imageFile = new File("generated_image.png");
+                try (FileOutputStream fileOutputStream = new FileOutputStream(imageFile)) {
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = imageStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+                return imageFile.getAbsolutePath();
+            }
+        }
+    }
 
     public String generateVideoFromScript(String script) throws IOException, InterruptedException {
         String scriptText = "Video Script:\n" + script;
@@ -59,8 +121,8 @@ public class VideoGenerationService {
         java.nio.file.Files.writeString(scriptFile.toPath(), scriptText);
 
         File videoFile = new File("generated_video.mp4");
-        String ffmpegCommand = String.format("/Users/syednoumanalishah/anaconda3/bin/ffmpeg -f lavfi -i color=c=blue:s=320x240:d=10 -vf \"drawtext=fontfile=/path/to/font.ttf: textfile=script.txt: fontsize=24: fontcolor=white: x=(w-text_w)/2: y=(h-text_h)/2\" -y %s",
-                videoFile.getAbsolutePath());
+        String ffmpegCommand = String.format(" C:/Users/raahi/anaconda3/envs/ffmpeg-env/Library/bin/ffmpeg -f lavfi -i color=c=blue:s=480x720:d=10 -vf \"drawtext=fontfile=/path/to/font.ttf: textfile=script.txt: fontsize=8: fontcolor=white: x=(w-text_w)/2: y=(h-text_h)/2\" -y %s",
+              videoFile.getAbsolutePath());
 
         ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", ffmpegCommand);
         processBuilder.redirectErrorStream(true);
